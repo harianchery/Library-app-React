b61824eb7126eb628c6c2554f93bcc45d4552b04
harianchery <hari.anchery001@gmail.com>
2024-06-07 14:21:23 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import AddBook from './components/AddBook';
 import SearchBook from './components/SearchBook';
 import DeleteBook from './components/DeleteBook';
 import Viewall from './components/Viewall';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddBook/>
-      <SearchBook/>
-      <DeleteBook/>
-      <Viewall/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddBook/>}/>
+      <Route path='/search' element={<SearchBook/>}/>
+      <Route path='/delete' element={<DeleteBook/>}/>
+      <Route path='/viewall' element={<Viewall/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,16 +12,16 @@ const Navbar = () => {
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Add new</a>
+          <a class="nav-link active" aria-current="page" href="/">Add new</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Search</a>
+          <a class="nav-link" href="/search">Search</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Delete</a>
+          <a class="nav-link" href="/delete">Delete</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">View </a>
+          <a class="nav-link" href="/viewall">View </a>
         </li>
       </ul>
     </div>
